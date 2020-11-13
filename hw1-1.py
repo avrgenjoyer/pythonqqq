063bec3dbbe5fb3afb9f7d3792a2df3bd043d57e
imverysorry <74419197+imverysorry@users.noreply.github.com>
2020-11-13 21:55:55 +0300

    Add files via upload
    Задача про сумму углов треугольника через класс.

---
+++ hw1-1.py
@@ -0,0 +1,15 @@
+class sum():
+    def __init__(self, X):
+        self.X=X
+    def Check(self, a, b, c):
+        self.a=a
+        self.b=b
+        self.c=c
+        if (self.a+self.b+self.c) == self.X:
+            return 1
+        else:
+            return 0
+
+
+xsum=sum(180)
+print(xsum.Check(int(input()), int(input()), int(input())))
