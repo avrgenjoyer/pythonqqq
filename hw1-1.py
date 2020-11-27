a1bff3739395983d9700700b877c0220634b4608
imverysorry <74419197+imverysorry@users.noreply.github.com>
2020-11-27 22:13:11 +0300

    Update hw1-1.py

--- hw1-1.py
+++ hw1-1.py
@@ -1,15 +1,7 @@
-class sum():
-    def __init__(self, X):
-        self.X=X
-    def Check(self, a, b, c):
-        self.a=a
-        self.b=b
-        self.c=c
-        if (self.a+self.b+self.c) == self.X:
-            return 1
-        else:
-            return 0
-
-
-xsum=sum(180)
-print(xsum.Check(int(input()), int(input()), int(input())))
+a=int(input())
+b=int(input())
+c=int(input())
+if a+b+c==180:
+    print("YES")
+else:
+    print("NO")
