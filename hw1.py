f55ba08ff695b7d9d13de09cae886045fe715487
imverysorry <74419197+imverysorry@users.noreply.github.com>
2020-11-27 22:14:35 +0300

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -2,10 +2,7 @@ class sum():
     def __init__(self, X):
         self.X=X
     def Check(self, a, b, c):
-        self.a=a
-        self.b=b
-        self.c=c
-        if (self.a+self.b+self.c) == self.X:
+        if (a+b+c) == self.X:
             return 1
         else:
             return 0
