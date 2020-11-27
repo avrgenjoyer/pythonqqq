f3981cbac6e18a8fb3ff6dda0ebe092100403f41
imverysorry <74419197+imverysorry@users.noreply.github.com>
2020-11-27 22:16:36 +0300

    Add files via upload

---
+++ hw11.py
@@ -0,0 +1,12 @@
+class sum():
+    def __init__(self, X):
+        self.X=X
+    def Check(self, a, b, c):
+        if (a+b+c) == self.X:
+            return 1
+        else:
+            return 0
+
+
+xsum=sum(180)
+print(xsum.Check(int(input()), int(input()), int(input())))
