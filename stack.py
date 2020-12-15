4b743095152f685e7dca992e1316f82abb951c01
imverysorry <74419197+imverysorry@users.noreply.github.com>
2020-12-15 22:47:21 +0300

    Add files via upload

--- stack.py
+++ stack.py
@@ -2,7 +2,7 @@ class Element:
     def __init__(self, value):
         self.value = value
         self.prev = None
-        self.next = None
+
 
 class Stack:
 
@@ -16,29 +16,31 @@ class Stack:
             self.head = element
         else:
             element.prev = self.head
-            self.head.next = element    
-            self.head = element        
+            self.head = element
 
         self.length += 1
-    
+
     def pop(self):
-        self.length -=1
+        self.length -= 1
         if self.head is None:
             return None
         else:
             k = self.head
             self.head = self.head.prev
-            return k
+            return k.value
+
     def top(self):
         return self.head.value
+
     def size(self):
         return self.length
+
     def is_empty(self):
-        if self.length !=0:
+        if self.length != 0:
             return False
         else:
             return True
+
     def clear(self):
-        while self.head != None:
-            self.head = self.head.prev
+        self.head = None
         self.length = 0
